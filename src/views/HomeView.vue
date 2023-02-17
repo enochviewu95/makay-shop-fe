af55aa970d161169cfa82c2cbfd30e681f1d3298
enochviewu95 <viewuenoch@gmail.com>
2023-02-17 11:28:09 +0000

    Added extra layouts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,8 @@
   <BestDealsLayoutComponent />
   <DiscountLayoutComponent />
   <WeeklyPopularProductsLayoutComponent />
+  <HeroLayoutComponent />
+  <TodaysBestDealLayoutComponent />
 </template>
 
 <script setup lang="ts">
@@ -12,6 +14,8 @@ import ShopCategoryLayoutComponent from "../components/layouts/ShopCategoryLayou
 import BestDealsLayoutComponent from "../components/layouts/BestDealsLayoutComponent.vue";
 import DiscountLayoutComponent from "../components/layouts/DiscountLayoutComponent.vue";
 import WeeklyPopularProductsLayoutComponent from "@/components/layouts/WeeklyPopularProductsLayoutComponent.vue";
+import HeroLayoutComponent from "@/components/layouts/HeroLayoutComponent.vue";
+import TodaysBestDealLayoutComponent from "@/components/layouts/TodaysBestDealLayoutComponent.vue";
 </script>
 
 <style scoped></style>
