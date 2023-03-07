1c1b738bc867f9cd62df9ae8e2cb3373e0f31903
enochviewu95 <viewuenoch@gmail.com>
2023-03-07 14:48:12 +0000

    Merged with master branch

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -25,7 +25,6 @@
               name="email"
               type="email"
               autocomplete="email"
-              required=""
               class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Email address"
             />
@@ -37,7 +36,6 @@
               name="password"
               type="password"
               autocomplete="current-password"
-              required=""
               class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Password"
             />
